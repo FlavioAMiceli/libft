c88774aa3e15bd05f5cb5da9d89021943d765759
Flavio Miceli <fmiceli@student.codam.nl>
2019-03-27 19:22:48 +0100

    Bug report: ft_memcmp not working due to unfinished optimization.

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -12,18 +12,72 @@
 
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
+static void				memcmp_unrolled(\
+	const unsigned long **str1, const unsigned long **str2, size_t *len)
 {
-	size_t			i;
+	while ((*len) / (4 * sizeof(unsigned long)) > 0)
+	{
+		if ((**str1) != (**str2))
+			break ;
+		*str1 += 1;
+		*str2 += 1;
+		(*len) -= sizeof(unsigned long);
+		if ((**str1) != (**str2))
+			break ;
+		*str1 += 1;
+		*str2 += 1;
+		(*len) -= sizeof(unsigned long);
+		if ((**str1) != (**str2))
+			break ;
+		*str1 += 1;
+		*str2 += 1;
+		(*len) -= sizeof(unsigned long);
+		if ((**str1) != (**str2))
+			break ;
+		*str1 += 1;
+		*str2 += 1;
+		(*len) -= sizeof(unsigned long);
+	}
+}
+
+static unsigned long	memcmp_wordcmp(\
+	const unsigned long **str1, const unsigned long **str2, size_t n)
+{
+	memcmp_unrolled(str1, str2, &n);
+	while (n / (sizeof(unsigned long)) > 0 && **str1 == **str2)
+	{
+		(*str1)++;
+		(*str2)++;
+		n -= sizeof(unsigned long);
+	}
+	return (n);
+}
+
+int					ft_memcmp(const void *s1, const void *s2, size_t n)
+{
 	unsigned char	*str1;
 	unsigned char	*str2;
 
-	if (n == 0)
-		return (0);
-	i = 0;
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-	while (str1[i] == str2[i] && i < n - 1)
-		i++;
-	return (str1[i] - str2[i]);
+	if (n >= sizeof(unsigned long) * 4)
+	{
+		while (((unsigned long)str1 & (sizeof(unsigned long) - 1)) != 0)
+		{
+			if (*str1 != *str2)
+				return (*str1 - *str2);
+			str1++;
+			str2++;
+			n--;
+		}
+		n = memcmp_wordcmp(\
+			(const unsigned long **)&str1, (const unsigned long **)&str2, n);
+	}
+	while (n > 0 && *str1 == *str2)
+	{
+		str1++;
+		str2++;
+		n--;
+	}
+	return (*str1 - *str2);
 }
